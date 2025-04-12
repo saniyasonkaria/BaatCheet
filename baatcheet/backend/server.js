8bd1b48e2f3091a6f6c5d4ab68d919482f364876
Saniya Sonkaria <saniya.sonkaria@waiin.com>
2025-04-12 23:13:22 +0530

    updated  dirname path

--- baatcheet/backend/server.js
+++ baatcheet/backend/server.js
@@ -36,18 +36,20 @@ app.use("/api/message", messageRoutes);
 // --------------------------deployment------------------------------
 
 const __dirname1 = path.resolve();
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../frontend/build")));
+  const buildPath = path.join(__dirname1, "..", "frontend", "build");
+
+  app.use(express.static(buildPath));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(buildPath, "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
-    res.send("API is running..");
+    res.send("API is running...");
   });
 }
+
 
 // --------------------------deployment------------------------------
 
