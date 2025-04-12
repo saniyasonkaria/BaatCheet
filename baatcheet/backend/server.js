35b0630ff35eb268a61fe2969a225a95a8ee9d82
Saniya Sonkaria <saniya.sonkaria@waiin.com>
2025-04-12 23:31:18 +0530

    "Resolved the build path issue"

--- baatcheet/backend/server.js
+++ baatcheet/backend/server.js
@@ -36,20 +36,18 @@ app.use("/api/message", messageRoutes);
 // --------------------------deployment------------------------------
 
 const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  const buildPath = path.join(__dirname1, "..", "frontend", "build");
 
-  app.use(express.static(buildPath));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(buildPath, "index.html"))
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
-    res.send("API is running...");
+    res.send("API is running..");
   });
 }
-
 
 // --------------------------deployment------------------------------
 
